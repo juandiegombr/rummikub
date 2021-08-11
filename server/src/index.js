f287e54a10b793e4702e8aa896fa2addf572a311
Juan Diego <juandiego.mbr@gmail.com>
2021-08-11 20:31:18 +0200

    [Added] Send shuffled tiles

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,58 @@
 const cors = require('cors')
 const express = require('express')
-const { generateGameCode } = require('./helpers')
-const app = express()
 const http = require('http')
-const server = http.createServer(app)
 const { Server } = require("socket.io")
+
+const TileService = require('./domain/tile')
+const { generateGameCode } = require('./helpers')
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app)
 const io = new Server(server)
 
 const PORT = 5000
+const GAMES = {}
 
-app.use(cors());
+const Socket = {
+  getById: (id) => io.sockets.sockets.get(id)
+}
 
-const games = {}
+const Room = {
+  getSockets: async (room) => {
+    const ids = await io.in(room).allSockets()
+    return [...ids].map((id) => {
+      return Socket.getById(id)
+    })
+  }
+}
+
+function shuffleTiles(socketsInRoom, game) {
+  socketsInRoom.forEach((playerSocket, index) => {
+    const gameTiles = GAMES[game.code].tiles
+    const positions = [
+      [0, 14],
+      [14, 28],
+    ]
+    playerSocket.emit('game:start', gameTiles.slice(...positions[index]))
+  })
+}
+
+async function afterUserJoined(game, socket) {
+  console.log('Websocket: ', game.code)
+
+  const roomName = `room:${game.code}`
+
+  socket.join(roomName)
+  socket.to(roomName).emit('user:joined')
+
+  const socketsInRoom = await Room.getSockets(roomName)
+
+  if (socketsInRoom.length === 2) {
+    shuffleTiles(socketsInRoom, game)
+  }
+}
 
 io.on('connection', (socket) => {
   console.log('Websocket: User connected')
@@ -22,10 +63,8 @@ io.on('connection', (socket) => {
     socket.join(`room:${game.code}`)
   })
 
-  socket.on('game:join', (game) => {
-    console.log('Websocket: ', game)
-    socket.to(`room:${game.code}`).emit('user:joined')
-    socket.join(`room:${game.code}`)
+  socket.on('game:join', async (game) => {
+    await afterUserJoined(game, socket)
   })
 
   socket.on('disconnect', () => {
@@ -50,14 +89,17 @@ app.get('/api/users/', (req, res) => {
 })
 
 app.get('/game/create/', (req, res) => {
-  const data = { code: generateGameCode() }
-  games[data.code] = data
+  const data = {
+    code: generateGameCode(),
+    tiles: TileService.shuffle(TileService.generateTiles()),
+  }
+  GAMES[data.code] = data
   res.json(data)
 })
 
 app.get('/game/join/:gameCode', (req, res) => {
   const data = { code: req.params.gameCode }
-  if (games[data.code]) {
+  if (GAMES[data.code]) {
     res.json(data)
     return
   }
